70b302c22db64a7661c18cf4f5d44978ebc23d24
Alexander Ogorodov <95526039+mrlexxo@users.noreply.github.com>
2022-04-04 15:33:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
 // Starting conditions
 
 let scores, currentScore, activePlayer, playing;
@@ -47,7 +52,30 @@ const switchPlayer = function () {
   player1El.classList.toggle('player--active');
 };
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 // Rolling dice functionality
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
 
 btnRoll.addEventListener('click', function () {
   // 1. Generate random dice roll
